6d5b56e2bb8bad95fe9e240278de74e677f18df7
Oscar <oscarjavierho@gmail.com>
2021-09-07 10:48:38 -0430

    Creando el formulario

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
@@ -6,6 +7,19 @@ function App() {
       <Header 
         titulo="Clima React App"
       />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario />
+            </div>
+            <div className="col m6 s12">
+              2
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Formulario.js
@@ -0,0 +1,32 @@
+const Formulario = () => {
+    return (
+        <form>
+                <div className="input-field col s12">
+                    <input
+                        type="text"
+                        name="ciudad"
+                        id="ciudad"
+                    />
+                    <label htmlFor="ciudad">Ciudad: </label>
+                </div>
+                <div className="input-field col s12">
+                    <select
+                        name="pais"
+                    >
+                        <option value="">Seleccione un país</option>
+                        <option value="US">Estados Unidos</option>
+                        <option value="MX">México</option>
+                        <option value="AR">Argentina</option>
+                        <option value="CO">Colombia</option>
+                        <option value="CR">Costa Rica</option>
+                        <option value="ES">España</option>
+                        <option value="PE">Perú</option>
+                        <option value="VE">Venezuela</option>
+                    </select>
+                    <label htmlFor="pais">País: </label>
+                </div>
+            </form>
+    );
+};
+
+export default Formulario;
